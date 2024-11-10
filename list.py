5f1c9d10fc359a7f1fd1fb3d4751efc665066513
Ayinka <babatundeakinyinka@gmail.com>
2024-11-10 15:47:21 +0100

    first commit

---
+++ list.py
@@ -0,0 +1,26 @@
+my_list =[]
+print(my_list)
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+my_list[1] =15
+print(my_list)
+
+new_list =[50,60,70]
+print(new_list)
+
+my_list.extend(new_list)
+print(my_list)
+
+my_list.pop(6)
+print(my_list)
+  
+my_list.sort()
+print(my_list)
+
+print(my_list[2])
+print(my_list)
